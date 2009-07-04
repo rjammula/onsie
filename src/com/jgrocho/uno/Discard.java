f958fb2b01fddb42c00ebf8eac11d276be094ec4
tangleforest <tangleforest@b3464f14-68c3-11de-8235-5b595ee06194>
2009-07-04 23:47:19 +0000

    Added copyright and licensing information

--- src/com/jgrocho/uno/Discard.java
+++ src/com/jgrocho/uno/Discard.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2009 Jonathan Grochowski
+ * 
+ * This file is part of onsie.
+ * 
+ * onsie is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * onsie is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with onsie.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
 package com.jgrocho.uno;
 
 import java.util.LinkedList;
