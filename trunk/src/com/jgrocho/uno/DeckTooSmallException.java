bac98109f917edbc6fb54113804d21f52c559cf4
tangleforest <tangleforest@b3464f14-68c3-11de-8235-5b595ee06194>
2009-07-07 03:24:01 +0000

    Removed line separator from message

--- trunk/src/com/jgrocho/uno/DeckTooSmallException.java
+++ trunk/src/com/jgrocho/uno/DeckTooSmallException.java
@@ -23,8 +23,7 @@ package com.jgrocho.uno;
 public class DeckTooSmallException extends RuntimeException {
     
     public DeckTooSmallException(int req, int size) {
-	super("Deck does not have enough cards to honor request" +
-	      System.getProperty("line.seperator") +
+	super("Deck does not have enough cards to honor request. " +
 	      req + " cards requested, only " + size + " cards available");
     }
 
